671ef9d98f846e8248da41575d78e91f03b81333
taeeunindaeyo <ktyy0402@naver.com>
2026-02-18 22:17:25 +0900

    Update joystick.c

--- Core/Src/joystick.c
+++ Core/Src/joystick.c
@@ -2,6 +2,7 @@
 #include "adc.h"
 
 #define JOY_CENTER 2048  // 12-bit ADC 중립값(대략)
+#define JOY_DEADZONE 150 
 
 // 조이스틱 입력 시작(= ADC DMA 변환 스타트)
 void Joystick_Init(void)
@@ -30,5 +31,14 @@ JoystickNorm Joystick_ReadNorm(void)
     JoystickNorm n;
     n.x = (int16_t)v[0] - JOY_CENTER;
     n.y = (int16_t)v[1] - JOY_CENTER;
+
+    // |150|보다 작으면 0으로 처리
+    if (raw_x > -JOY_DEADZONE && raw_x < JOY_DEADZONE) n.x = 0;
+    else n.x = raw_x;
+
+    if (raw_y > -JOY_DEADZONE && raw_y < JOY_DEADZONE) n.y = 0;
+    else n.y = raw_y;
+    
     return n;
 }
+
