e76aef314b51b70392bc74c63dee5626d02abd88
taeeunindaeyo <ktyy0402@naver.com>
2026-02-27 14:42:26 +0900

    Update towerlamp.h

--- RX_Core/Inc/towerlamp.h
+++ RX_Core/Inc/towerlamp.h
@@ -5,16 +5,16 @@
 
 // RED : PA8 , YELLOW : PA9 , BLUE : PA10
 
-#define RLED_ON()     (GPIOA->BSRR = (1U << 8))
-#define RLED_OFF()    (GPIOA->BSRR = (1U << (8  + 16)))
+#define RLED_ON()     (GPIOA->BSRR = (1U << 9))
+#define RLED_OFF()    (GPIOA->BSRR = (1U << (9  + 16)))
 
-#define YLED_ON()     (GPIOA->BSRR = (1U << 9))
-#define YLED_OFF()    (GPIOA->BSRR = (1U << (9  + 16)))
+#define YLED_ON()     (GPIOA->BSRR = (1U << 10))
+#define YLED_OFF()    (GPIOA->BSRR = (1U << (10  + 16)))
 
-#define BLED_ON()     (GPIOA->BSRR = (1U << 10))
-#define BLED_OFF()    (GPIOA->BSRR = (1U << (10 + 16)))
+#define BLED_ON()     (GPIOA->BSRR = (1U << 11))
+#define BLED_OFF()    (GPIOA->BSRR = (1U << (11 + 16)))
 
 void towerlamp_init(void);
 void towerlamp_all_off(void);
 
-#endif 
+#endif
